3ee9d488134124901e2c7ac9f3e59086ed2958c6
Jennifer Bland <ratracegrad@gmail.com>
2016-05-19 22:23:34 -0400

    moved scripts into separate file

---
+++ public/javascripts/myscript.js
@@ -0,0 +1,65 @@
+$(document).ready(function() {
+
+    var margin = {top: 20, right: 20, bottom: 30, left: 40},
+        width = 960 - margin.left - margin.right,
+        height = 500 - margin.top - margin.bottom;
+
+    var x = d3.scale.ordinal()
+            .rangeRoundBands([0, width], .1);
+
+    var y = d3.scale.linear()
+            .range([height, 0]);
+
+    var xAxis = d3.svg.axis()
+            .scale(x)
+            .orient("bottom");
+
+    var yAxis = d3.svg.axis()
+            .scale(y)
+            .orient("left")
+            .ticks(10, "%");
+
+    var svg = d3.select("body").append("svg")
+            .attr("width", width + margin.left + margin.right)
+            .attr("height", height + margin.top + margin.bottom)
+            .append("g")
+            .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
+
+    d3.json('GDP-data.json', function(error, results) {
+        if (error) throw error;
+
+        var data = results.data;
+
+        x.domain(data.map(function(d) { return d[0]; }));
+        y.domain([0, d3.max(data, function(d) { return d[1]; })]);
+
+        svg.append("g")
+                .attr("class", "x axis")
+                .attr("transform", "translate(0," + height + ")")
+                .call(xAxis);
+
+        svg.append("g")
+                .attr("class", "y axis")
+                .call(yAxis)
+                .append("text")
+                .attr("transform", "rotate(-90)")
+                .attr("y", 6)
+                .attr("dy", ".71em")
+                .style("text-anchor", "end")
+                .text(results.code);
+
+        svg.selectAll(".bar")
+                .data(data)
+                .enter().append("rect")
+                .attr("class", "bar")
+                .attr("x", function(d) { return x(d[0]); })
+                .attr("width", x.rangeBand())
+                .attr("y", function(d) { return y(d[1]); })
+                .attr("height", function(d) { return height - y(d[1]); });
+    });
+})
+
+function type(d) {
+    d[1] = +d[1];
+    return d;
+}
